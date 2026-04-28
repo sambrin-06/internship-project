2441be78ee25ffe9b186bccd78859ccf9f403562
internship-training-cp-html <sambrins069@gmail.com>
2026-04-28 12:13:51 +0530

    first commit

---
+++ internal-controls-testing/src/test/java/com/ict/internal_controls_testing/InternalControlsTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict.internal_controls_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalControlsTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
